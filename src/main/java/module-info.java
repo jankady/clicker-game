06639fc1006c2ad7283f8bb318e86958b919cbd3
jankady <kaduch05@gmail.com>
2023-05-05 22:00:30 +0200

    creating and importing media library in GameController.java
    Signed-off-by: jankady <kaduch05@gmail.com>

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -2,6 +2,7 @@ module cz.kaduch.clickergame {
     requires javafx.controls;
     requires javafx.fxml;
     requires java.sql;
+    requires javafx.media;
 
 
     opens cz.kaduch.clickergame to javafx.fxml;
